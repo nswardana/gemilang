3bb653acc83c35ff3bb121dc6e5f35adee4d9a10
Nanang Setya Wardana <nswardana@gmail.com>
2025-11-07 22:10:51 +0700

    no message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 6173, // ⬅️ fix port di sini
+    port: 6173,
     host: true,
+    allowedHosts: [
+      "gemilang.beeasy.id",
+      "www.gemilang.beeasy.id",
+    ],
   },
 });
