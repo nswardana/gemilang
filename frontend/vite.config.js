2d71ef5b90694df60ee21842e511a49707ced287
Nanang Setya Wardana <nswardana@gmail.com>
2025-11-07 18:52:56 +0700

    port 6173

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // ⬅️ fix port di sini
+    port: 6173, // ⬅️ fix port di sini
     host: true,
   },
 });
